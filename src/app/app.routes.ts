3f843c56117f2140dcb9fa310283ab6c964ff6b2
Sodany Sean <champloo63@gmail.com>
2024-12-05 12:43:26 +0100

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CampaignComponent } from './campaign/campaign.component';
+import { CreatecampaignpageComponent } from './createcampaignpage/createcampaignpage.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'campaign', component: CampaignComponent }
-
 ];
 
+export const routesCampaign: Routes = [
+  { path: 'campaign/create', component: CreatecampaignpageComponent }
+];  
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),           // Importation des routes principales
+    RouterModule.forChild(routesCampaign)  // Importation des routes supplémentaires pour la création de campagne
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/campaign-menu/campaign-menu.component.ts
@@ -0,0 +1,15 @@
+// campaign-menu.component.ts
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-campaign-menu',
+  imports: [RouterOutlet, RouterLink],
+
+  templateUrl: './campaign-menu.component.html',
+  styleUrls: ['./campaign-menu.component.css']
+})
+export class CampaignMenuComponent {
+
+}
